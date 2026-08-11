05afdd554afe43d099fc404cc2583adc255894eb
Kushlik103 <kushlikkumar1@gmail.com>
2026-08-11 12:07:58 +0500

    Update sketch_aug3g.ino

--- sketch_aug3g.ino
+++ sketch_aug3g.ino
@@ -1,23 +1,43 @@
-int pirPin = 9;      // PIR sensor output
-int ledPin = 13;     // LED pin
+#include <Servo.h>
+
+const int trigPin = 9;
+const int echoPin = 10;
+const int servoPin = 6;
+
+Servo lidServo;
+
+long duration;
+int distance;
 
 void setup() {
-  pinMode(pirPin, INPUT);
-  pinMode(ledPin, OUTPUT);
-
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  lidServo.attach(servoPin);
+  lidServo.write(0);   // Lid closed position
   Serial.begin(9600);
 }
 
 void loop() {
-  int motion = digitalRead(pirPin);
 
-  if (motion == HIGH) {
-    digitalWrite(ledPin, HIGH);
-    Serial.println("Motion Detected!");
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration * 0.034 / 2;
+
+  Serial.print("Distance: ");
+  Serial.println(distance);
+
+  if (distance < 10) {
+    lidServo.write(90);  // Open lid
+    delay(3000);          // Keep it open briefly
   } else {
-    digitalWrite(ledPin, LOW);
-    Serial.println("No Motion");
+    lidServo.write(0);   // Close lid
   }
 
-  delay(200);
+  delay(100);
 }
